d179d101c731945ce47c5f1f7558e0b0203475a2
Genesis Tama <genesistama67@gmail.com>
2026-03-17 18:07:34 +0100

    database fix: price is now nullable

---
+++ database/migrations/2026_03_17_170050_make_price_nullable_on_designs_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+   public function up(): void
+{
+    Schema::table('designs', function (Blueprint $table) {
+        // This tells the database to allow nulls for this column
+        $table->decimal('price', 10, 2)->nullable()->change();
+    });
+}
+
+public function down(): void
+{
+    Schema::table('designs', function (Blueprint $table) {
+        $table->decimal('price', 10, 2)->nullable(false)->change();
+    });
+}
+};
