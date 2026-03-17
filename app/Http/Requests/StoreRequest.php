7211fbe196f0104389bfdc1e483c2ef6f4695377
Genesis Tama <genesistama67@gmail.com>
2026-03-17 16:53:54 +0100

    made price filed nullable

--- app/Http/Requests/StoreRequest.php
+++ app/Http/Requests/StoreRequest.php
@@ -25,7 +25,7 @@ class StoreRequest extends FormRequest
             //
             'design_Name'=>'required|string|max:25',
             'description'=>'required',
-            'price'=>'required|numeric|min:0',
+            'price'=>'numeric|nullable|min:0',
             'category'=>'required|in:Gowns,Two-Piece,Wedding-Attire,ready-to-wear,Dinner-Wear',
             'fabric'=>'required|string',
             'size'=>'required|string',
